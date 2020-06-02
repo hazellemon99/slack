dde52b1115a38e6355d276cdbc8fc04246b56f84
hazellemon99 <64386922+hazellemon99@users.noreply.github.com>
2020-06-01 23:20:39 -0400

    Update LogCall.java

--- src/day_1/LogCall.java
+++ src/day_1/LogCall.java
@@ -3,5 +3,7 @@ package day_1;
 public class LogCall {
     public static void main(String[] args) {
         System.out.println("LogCall assignment");
+        
+        //this is from Github
     }
 }
